7009386510588435bd1fd63cacdf4c0165e66b5c
Vinicius Milani Rodrigues de Freitas <vmrfreitas@inf.ufrgs.br>
2019-11-08 01:02:54 -0300

    Tutorial part 3

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -7,11 +7,13 @@ public class PlayerMovement : MonoBehaviour
     public float speed;
     private Rigidbody2D myRigidbody;
     private Vector3 change;
+    private Animator animator;
 
     // Start is called before the first frame update
     void Start()
     {
         myRigidbody = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
 
     }
 
@@ -21,9 +23,7 @@ public class PlayerMovement : MonoBehaviour
         change = Vector3.zero;
         change.x = Input.GetAxisRaw("Horizontal");
         change.y = Input.GetAxisRaw("Vertical");
-        if (change != Vector3.zero){
-            MoveCharacter();
-        }
+        UpdateAnimationAndMove();
     }
 
     void MoveCharacter()
@@ -31,5 +31,17 @@ public class PlayerMovement : MonoBehaviour
         myRigidbody.MovePosition(
             transform.position + change.normalized * speed * Time.deltaTime
             );
+    }
+
+    void UpdateAnimationAndMove()
+    {
+        if (change != Vector3.zero){
+            MoveCharacter();
+            animator.SetFloat("moveX", change.x);
+            animator.SetFloat("moveY", change.y);
+            animator.SetBool("moving", true);
+        } else {
+            animator.SetBool("moving", false);
+        }
     }
 }
