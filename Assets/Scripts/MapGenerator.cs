f377fdb8b360b683663747a0a56fc1a1fa0642f7
vmrfreitas <vmrfreitas@inf.ufrgs.br>
2021-03-11 23:17:00 -0300

    commit 1 lol

---
+++ Assets/Scripts/MapGenerator.cs
@@ -0,0 +1,222 @@
+﻿using UnityEngine;
+using UnityEngine.Tilemaps;
+using System.Collections;
+using System;
+
+public class MapGenerator : MonoBehaviour {
+	private int debugcounter=0;
+	private const int centerX = 27;
+	private const int centerY = 8;
+	private int areaCounter = 10;
+	public int width;
+	public int height;
+	private int halfWidth;
+	private int halfHeight;
+	private	int x=0, y=0, z=0;
+	public int iteractions;
+	public string seed;
+	public bool useRandomSeed;
+	public Tile ground;
+	public Tile wall;
+	public Tilemap tileMap;
+
+	[Range(0,100)]
+	public int randomFillPercent;
+
+	int[,] map;
+	int[,] halfMap;
+	int[,] halfMap2;
+	void Start() {
+		GenerateMap();
+	}
+
+	void DrawGround() {
+		for (int x = -25; x < 10; x ++) {
+			for (int y = -7; y < 23; y ++) {
+				tileMap.SetTile(new Vector3Int(x, y, 0), ground);
+			}
+		}
+	}
+
+	void Update() {
+		if (Input.GetMouseButtonDown(0)) {
+			GenerateMap();
+		}
+
+		if(Input.GetKeyDown(KeyCode.Space)){
+			DrawGround();
+			Debug.Log("asdadasd");
+			x+=1;
+			y+=1;
+			z+=1;
+		}
+	}
+
+	void GenerateMap() {
+		map = new int[width,height];
+		halfHeight = (int)(height/2);
+		halfWidth = (int)(width/2);
+		Debug.Log(halfHeight);
+		Debug.Log(halfWidth);
+		halfMap = new int[halfWidth,halfHeight];
+		halfMap2 = new int[halfWidth,halfHeight];
+		RandomFillMap();
+		for (int i = 0; i < iteractions; i ++) {
+			SmoothMap();
+			halfMap = (int[,])halfMap2.Clone();
+		}
+		//doubleSize();
+		//findWallsAndAreas(); // pegar coordenada de todos os pontos em uma area, pegar quais são as paredes
+		//connectAllAreas(); // fazer distancia de todas as paredes entre todas as paredes até achar o mais próximo pra conectar
+	}
+
+	void doubleSize() {
+		for (int x = 1; x < width-1; x ++) {
+			for (int y = 1; y < height-1; y ++) {
+				if (map[x-1,y]==0 && map[x,y]==1 && map[x+1,y] == 0){
+					map[x,y] = 0;
+				} else if (map[x-1,y]==0 && map[x,y]==1 && map[x+1,y] == 0){
+					map[x,y] = 0;
+				}
+			}
+		}
+	}
+
+	void connectAllAreas() {
+		
+	}
+	
+	void findWallsAndAreas() {
+		areaCounter = 10;
+		//this for the areas
+		for (int x = 0; x < width; x ++) {
+			for (int y = 0; y < height; y ++) {
+				if (map[x,y] == 0){
+					Debug.Log(debugcounter);
+					Debug.Log(map);
+					debugcounter++;
+					paintBucket(x,y);
+					areaCounter++;
+				}
+			}
+		}
+	}
+
+	void paintBucket(int x, int y){
+		map[x,y] = areaCounter;
+		if (map[x+1,y] != 1 && map[x+1,y] != areaCounter && x+1 < width){
+			paintBucket(x+1, y);
+		}
+		if (map[x,y+1] != 1 && map[x,y+1] != areaCounter && y+1 < height){
+			paintBucket(x, y+1);
+		} 
+		if (map[x-1,y] != 1 && map[x-1,y] != areaCounter && x-1 > 0){
+			paintBucket(x-1, y);
+		}
+		if (map[x,y-1] != 1 && map[x,y-1] != areaCounter && y-1 > 0){
+			paintBucket(x,y-1);
+		}
+		return;
+	}
+
+	void RandomFillMap() {
+		if (useRandomSeed) {
+			seed = Time.time.ToString();
+		}
+
+		System.Random pseudoRandom = new System.Random(seed.GetHashCode());
+
+		for (int x = 0; x < halfWidth; x ++) {
+			for (int y = 0; y < halfHeight; y ++) {
+				if (x == 0 || x == halfWidth-1 || y == 0 || y == halfHeight -1) {
+					halfMap[x,y] = 1;
+				}
+				else {
+					halfMap[x,y] = (pseudoRandom.Next(0,100) < randomFillPercent)? 1: 0;
+				}
+				halfMap2[x,y] = halfMap[x,y];
+			}
+		}
+	}
+
+	void SmoothMap() {
+		for (int x = 0; x < halfWidth; x ++) {
+			for (int y = 0; y < halfHeight; y ++) {
+				int neighbourWallTiles = GetSurroundingWallCount(x,y);
+
+				if (neighbourWallTiles > 4)
+					halfMap2[x,y] = 1;
+				else if (neighbourWallTiles < 4)
+					halfMap2[x,y] = 0;
+
+			}
+		}
+	}
+
+	int GetSurroundingWallCount(int gridX, int gridY) {
+		int wallCount = 0;
+		for (int neighbourX = gridX - 1; neighbourX <= gridX + 1; neighbourX ++) {
+			for (int neighbourY = gridY - 1; neighbourY <= gridY + 1; neighbourY ++) {
+				if (neighbourX >= 0 && neighbourX < halfWidth && neighbourY >= 0 && neighbourY < halfHeight) {
+					if (neighbourX != gridX || neighbourY != gridY) {
+						wallCount += halfMap[neighbourX,neighbourY];
+					}
+				}
+				else {
+					wallCount ++;
+				}
+			}
+		}
+
+		return wallCount;
+	}
+/*
+	void OnDrawGizmos() {
+		if (map != null) {
+			for (int x = 0; x < width; x ++) {
+				for (int y = 0; y < height; y ++) {
+					Gizmos.color = getGizmoColor(x,y);
+					Vector3 pos = new Vector3(-width/2 + x + centerX + .5f,-height/2 + y + centerY +.5f, 0);
+					Gizmos.DrawCube(pos,Vector3.one);
+				}
+			}
+		}
+	}
+*/
+	void OnDrawGizmos() {
+		if (halfMap != null) {
+			for (int x = 0; x < halfWidth; x ++) {
+				for (int y = 0; y < halfHeight; y ++) {
+					Gizmos.color = getGizmoColor(x,y);
+					Vector3 pos = new Vector3(-halfWidth/2 + x + centerX + .5f,-halfHeight/2 + y + centerY +.5f, 0);
+					Gizmos.DrawCube(pos,Vector3.one);
+				}
+			}
+		}
+	}
+
+	Color getGizmoColor(int x, int y) {
+		if (halfMap[x,y] == 1) {
+			return Color.black;
+		}
+		if (halfMap[x,y] == 0) {
+			return Color.white;
+		}
+		if (halfMap[x,y] == 10){
+			return Color.red;
+		}
+		if (halfMap[x,y] == 11){
+			return Color.blue;
+		}
+		if (halfMap[x,y] == 12){
+			return Color.green;
+		}
+		if (halfMap[x,y] == 13){
+			return Color.cyan;
+		}
+		if (halfMap[x,y] == 14){
+			return Color.yellow;
+		}
+		return Color.magenta;
+	}
+}
--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -2,8 +2,15 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+public enum PlayerState{
+    walk,
+    attack,
+    interact
+}
+
 public class PlayerMovement : MonoBehaviour
 {
+    public PlayerState currentState;
     public float speed;
     private Rigidbody2D myRigidbody;
     private Vector3 change;
@@ -12,9 +19,11 @@ public class PlayerMovement : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        currentState = PlayerState.walk;
         myRigidbody = GetComponent<Rigidbody2D>();
         animator = GetComponent<Animator>();
-
+        animator.SetFloat("moveX", 0);
+        animator.SetFloat("moveY", -1);
     }
 
     // Update is called once per frame
@@ -23,11 +32,26 @@ public class PlayerMovement : MonoBehaviour
         change = Vector3.zero;
         change.x = Input.GetAxisRaw("Horizontal");
         change.y = Input.GetAxisRaw("Vertical");
-        UpdateAnimationAndMove();
+        if(Input.GetButtonDown("attack") && currentState != PlayerState.attack){
+            StartCoroutine(AttackCo());
+        } else if(currentState == PlayerState.walk){
+            UpdateAnimationAndMove();
+        }
+    }
+
+    private IEnumerator AttackCo()
+    {
+        animator.SetBool("attacking", true);
+        currentState = PlayerState.attack;
+        yield return null;
+        animator.SetBool("attacking", false);
+        yield return new WaitForSeconds(.3333333333333333f);
+        currentState = PlayerState.walk;
     }
 
     void MoveCharacter()
     {
+        change.Normalize();
         myRigidbody.MovePosition(
             transform.position + change.normalized * speed * Time.deltaTime
             );
